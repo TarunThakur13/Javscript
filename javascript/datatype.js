baf62e06eadb400f00fd010fa6210c72eb58a1a5
TarunThakur13 <159451831+TarunThakur13@users.noreply.github.com>
2024-06-10 14:56:14 +0000

    datatype

---
+++ javascript/datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3);  we are using nodejs , not browser(isme alert nahi chalta)
+
+console.log(3+3); 
+console.log("hitesh")  // dono chalte hai
+
+let name="tarun"
+let age=3
+let isLoggenIn=true
+
+// Primitive data types
+// number => 2 to power 53
+// bigint (for big numbers ex. stocks)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// objects
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
