0df51e4801feca615982b2222ac47c5684faae5a
TarunThakur13 <159451831+TarunThakur13@users.noreply.github.com>
2024-06-12 16:59:06 +0000

    objects,api

--- basics2/object2.js
+++ basics2/object2.js
@@ -14,8 +14,8 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser);
-console.log(regularUser.fullname?.userfullname.firstname); // ? checks whether key exist
+// console.log(regularUser);
+// console.log(regularUser.fullname?.userfullname.firstname); // ? checks whether key exist
 
 const obj1 = {
     1:"a",2:"b"
@@ -31,12 +31,12 @@ const obj6 = {
 
 // const obj3 = {obj1,obj2}
 const obj3 = Object.assign(obj1,obj2,obj6)
-console.log(obj3);
+// console.log(obj3);
 
-console.log(obj1); // obj1 and obj3 same
+// console.log(obj1); // obj1 and obj3 same
 
 const obj4 = Object.assign({},obj1,obj2,obj6)
-console.log(obj4); // same but assures the joining {} act as target others are joined as sources
+// console.log(obj4); // same but assures the joining {} act as target others are joined as sources
 
 
 const obj9 = {...obj1,...obj2}
@@ -51,15 +51,33 @@ const users = {
 
 // users[1].email 
 
-console.log(gitUser);
-console.log(Object.keys(gitUser));
-console.log(Object.values(gitUser));
-console.log(Object.entries(gitUser));
+// console.log(gitUser);
+// console.log(Object.keys(gitUser));
+// console.log(Object.values(gitUser));
+// console.log(Object.entries(gitUser));
 
-console.log(gitUser.hasOwnProperty('name'));
-
-
+// console.log(gitUser.hasOwnProperty('name'));
 
 
+const course = {
+    courseName : "JS",
+    price : "999",
+    courseInstructor : "Tarun"
+}
 
+const {courseInstructor: cours} = course
+console.log(cours);
+ 
+// JSON
+// {
+//     "name" : "Tarun",
+//     "courseName" : "JS",
+//     "price" : "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
 
