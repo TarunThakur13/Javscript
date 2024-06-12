505a262f6ad586770448763a7a8485cfad456849
TarunThakur13 <159451831+TarunThakur13@users.noreply.github.com>
2024-06-12 11:02:41 +0000

    arrays

--- basics2/arrays.js
+++ basics2/arrays.js
@@ -42,5 +42,26 @@ const myn2 = myArr.splice(1,3)
 
 const marvel_heros = ["thor","ironman","spiderman"]
 const dc = ["superman","flash","batman"]
-marvel_heros.push(dc)
+// marvel_heros.push(dc)
 console.log(marvel_heros);
+
+
+const allHeros = marvel_heros.concat(dc)
+// console.log(allHeros);
+
+const all_heros = [...marvel_heros, ...dc]
+// console.log(all_heros);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("tarun"))
+console.log(Array.from("tarun"))
+console.log(Array.from({name:"tarun"})) // not specified key ya value ka array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3,score3));
